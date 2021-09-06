52a002c544ca0bb8caf89f2d3eb32eb335dff4ca
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-06 12:18:20 -0500

    type

---
+++ 0906_comandos_basicos/el_type.py
@@ -0,0 +1,6 @@
+# Pedir los datos al usuario
+numero = eval(input("Escribe un número: "))
+texto = input("Escribe texto: ")
+
+print("el tipo de numero es: ", type(numero))
+print("El tipo de text es: ", type(texto))
