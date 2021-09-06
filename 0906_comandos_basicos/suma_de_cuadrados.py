f53e9e32df9636415ef5fc9fb23972f8adcda153
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-06 12:23:37 -0500

    suma de cuadrados

---
+++ 0906_comandos_basicos/suma_de_cuadrados.py
@@ -0,0 +1,9 @@
+# Pedir los datos al usuario
+num1 = eval(input("Escribe el primer número: "))
+num2 = eval(input("Escribe el segundo número: "))
+
+# Realizo la operación
+suma = num1**2 + num2**2
+
+# Imprimo el resultado
+print("La suma de los cuadrados es", suma)
