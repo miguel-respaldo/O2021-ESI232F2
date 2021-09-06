5ef7b7d18a81f95c59d70bede7a8577ad87d69d3
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-06 11:46:44 -0500

    suma

---
+++ 0906_comandos_basicos/suma.py
@@ -0,0 +1,8 @@
+# Pedir los datos al usuario
+num1 = eval(input("Escribe el primer número: "))
+num2 = eval(input("Escribe el segundo número: "))
+
+# Realizo la operación
+suma = num1 + num2
+
+print("El resultado es", suma)
