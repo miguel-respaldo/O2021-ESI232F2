a83ec4b7834d84e8dd39b432eb6a3c14d7474ed4
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-08 11:44:07 -0500

    cadenas de texto

---
+++ 0908_cadenas-de-texto/01-strings.py
@@ -0,0 +1,11 @@
+s1 = str() #crea un objeto vacío
+print("El valor es: ", s1)
+
+s1 = ""  #  lo mismo que s1 = str()
+print("El valor es: ", s1)
+
+s2 = "Libreta"
+print("El valor es: ", s2)
+
+s2 = str("Libreta")
+print("El valor es: ", s2)
