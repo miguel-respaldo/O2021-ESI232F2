5fa6c22a0674e35f89f8df5c9abd576e42c08d9d
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-08 12:20:08 -0500

    comparaciones de texto

---
+++ 0908_cadenas-de-texto/03-comparando-cadenas.py
@@ -0,0 +1,11 @@
+# para los números
+print(5 < 10)
+print(5 <= 10)
+print(5 > 10)
+print(5 >= 10)
+print(5 == 10)
+print(5 != 10)
+print("--------------------")
+# las cadenas de texto
+
+print("a" < "A")
