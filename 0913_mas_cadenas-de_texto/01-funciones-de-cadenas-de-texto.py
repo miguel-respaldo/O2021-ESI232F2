0a3cb73bc5da36077341fc2e96fc90dd4d6b76a9
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-13 11:27:41 -0500

    funciones de cadena

---
+++ 0913_mas_cadenas-de_texto/01-funciones-de-cadenas-de-texto.py
@@ -0,0 +1,31 @@
+s = "Hola Mundo"
+print("La cadena es:", s)
+print("isdigit:",s.isdigit())
+print("isalpha:",s.isalpha())
+print("isupper:",s.isupper())
+print("islower:",s.islower())
+print("---------------------")
+
+s = "hola mundo"
+print("La cadena es:", s)
+print("isdigit:",s.isdigit())
+print("isalpha:",s.isalpha())
+print("isupper:",s.isupper())
+print("islower:",s.islower())
+print("---------------------")
+
+s = "HOLAMUNDO"
+print("La cadena es:", s)
+print("isdigit:",s.isdigit())
+print("isalpha:",s.isalpha())
+print("isupper:",s.isupper())
+print("islower:",s.islower())
+print("---------------------")
+
+s = "123"
+print("La cadena es:", s)
+print("isdigit:",s.isdigit())
+print("isalpha:",s.isalpha())
+print("isupper:",s.isupper())
+print("islower:",s.islower())
+print("---------------------")
