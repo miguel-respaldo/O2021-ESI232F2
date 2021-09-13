0e646d44e1adbd20a457fe6421b0a06056c748a9
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-13 11:52:16 -0500

    buscar cadena

---
+++ 0913_mas_cadenas-de_texto/02-buscando_cadenas.py
@@ -0,0 +1,8 @@
+s = 'Bienvenido a Python'
+#    0123456789012345678
+
+print("Terimina con thon?",s.endswith("thon"))
+print("Comienza con Buen?",s.startswith("Buen"))
+print("La posicion de ido es:",s.find("ido"))
+print("La posicion de venida es:",s.rfind("venida"))
+print("Hay ",s.count("o")," 'o' en la cadena")
