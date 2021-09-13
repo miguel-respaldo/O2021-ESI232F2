087b5a83da2fff837683efda06ecefad23a4c083
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-13 12:23:15 -0500

    ejercicio cuanta vocales

---
+++ 0913_mas_cadenas-de_texto/04-Ejercicio_contardor_de_vocales.py
@@ -0,0 +1,9 @@
+print("Programa que cuenta vocales")
+
+palabra = input("Escribe lo que sea:")
+
+print("Hay ", palabra.count("a")," 'a' en la cadena")
+print("Hay ", palabra.count("e")," 'e' en la cadena")
+print("Hay ", palabra.count("i")," 'i' en la cadena")
+print("Hay ", palabra.count("o")," 'o' en la cadena")
+print("Hay ", palabra.count("u")," 'u' en la cadena")
