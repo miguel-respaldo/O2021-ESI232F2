b7e3bc2abe0a73b3f36bf7d4fc5a3385ec88419f
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-15 12:15:13 -0500

    cadenas y subcadenas 1

---
+++ 0915_ejercicios_cadenas/01-subcadenas.py
@@ -0,0 +1,7 @@
+frase1 = "Esta es una frase que no dice nada"
+frase2 = "Hola"
+
+# Para saber el largo de una cadena de texto se usa "len"
+
+print("La frase 1 tiene de largo:", len(frase1))
+print("La frase 2 tiene de largo:", len(frase2))
