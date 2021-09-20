dc0550303bae1f02f5e7ae49193c13bc96dbad9f
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-20 11:39:15 -0500

    mayusculas y minusculas 3

--- 0920-funciones_de_cadena/01-todas_mayusculas_o_minusculas.py
+++ 0920-funciones_de_cadena/01-todas_mayusculas_o_minusculas.py
@@ -2,3 +2,6 @@ frase = input("Escribe una frase: ")
 
 print("La frase en mayusculas es " + frase.upper())
 print("La frase en minusculas es " + frase.lower())
+
+mayusculas = frase.upper()
+print(mayusculas[3])
