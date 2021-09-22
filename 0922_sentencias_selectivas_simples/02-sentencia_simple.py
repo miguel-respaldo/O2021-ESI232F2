3aeeb6428931bcbc5ed7211df78804081f0819fd
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-22 12:28:50 -0500

    sentencia simple 2

---
+++ 0922_sentencias_selectivas_simples/02-sentencia_simple.py
@@ -0,0 +1,9 @@
+#Escribir "Ingresa su edad"
+#Leer edad
+edad = eval(input("Ingresa tu edad: "))
+
+#Si edad >= 18 Entonces
+if edad >= 18:
+  #Escribir "Ya eres mayor de edad"
+  print("Ya eres mayor de edad.")
+#Fin Si
---
+++ 0922_sentencias_selectivas_simples/03-sentencia_simple_limpio.py
@@ -0,0 +1,5 @@
+
+edad = eval(input("Ingresa tu edad: "))
+
+if edad >= 18:
+  print("Ya eres mayor de edad.")
