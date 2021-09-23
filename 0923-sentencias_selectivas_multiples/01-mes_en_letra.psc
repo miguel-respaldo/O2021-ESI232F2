75d1d8cafce7e86cfda8a5f4a29b7567e89597bd
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-23 16:34:54 -0500

    sentencia multiple

---
+++ 0923-sentencias_selectivas_multiples/01-mes_en_letra.psc
@@ -0,0 +1,35 @@
+Algoritmo mes_en_letra
+	
+	Escribir "Escribe el número de un mes : "
+	Leer mes
+	
+	Segun mes Hacer
+		1:
+			Escribir "el mes es Enero"
+		2:
+			Escribir "el mes es Febrero"
+		3:
+			Escribir "el mes es Marzo"
+		4:
+			Escribir "el mes es Abril"
+		5:
+			Escribir "el mes es Mayo"
+		6:
+			Escribir "el mes es Junio"
+		7:
+			Escribir "el mes es Julio"
+		8:
+			Escribir "el mes es Agosto"
+		9:
+			Escribir "el mes es Septiembre"
+		10:
+			Escribir "el mes es Octubre"
+		11:
+			Escribir "el mes es Noviembre"
+		12:
+			Escribir "el mes es Diciembre"
+		De Otro Modo:
+			Escribir "El mes no existe"
+	Fin Segun
+	
+FinAlgoritmo
---
+++ 0923-sentencias_selectivas_multiples/02-mes_en_letra.py
@@ -0,0 +1,31 @@
+mes = eval(input("Escribe el número de mes: "))
+
+# Un igual (=)   es asignación
+# Dos igual (==) es comparación
+
+if mes == 1:
+    print("el mes es Enero")
+elif mes == 2:
+    print("el mes es Febrero")
+elif mes == 3:
+    print("el mes es Marzo")
+elif mes == 4:
+    print("el mes es Abril")
+elif mes == 5:
+    print("el mes es Mayo")
+elif mes == 6:
+    print("el mes es Junio")
+elif mes == 7:
+    print("el mes es Julio")
+elif mes == 8:
+    print("el mes es Agosto")
+elif mes == 9:
+    print("el mes es Septiembre")
+elif mes == 10:
+    print("el mes es Octubre")
+elif mes == 11:
+    print("el mes es Noviembre")
+elif mes == 12:
+    print("el mes es Diciembre")
+else:
+    print("el mes no existe")
