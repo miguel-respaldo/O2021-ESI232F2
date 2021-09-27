c649ce83d241d22eee131e6fc804bdc39f0c920f
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-27 12:25:26 -0500

    mayor de 3

---
+++ 0927-sentencias_selectivas/04-mayor_de_tres.py
@@ -0,0 +1,8 @@
+num1 = eval(input("Escribe el primer número: "))
+num2 = eval(input("Escribe el primer número: "))
+num3 = eval(input("Escribe el primer número: "))
+
+if num1 > num2:
+    pass
+else:
+    pass
