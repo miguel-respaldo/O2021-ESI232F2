1879e945db70feaafe953e529898a2db9e011572
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-29 12:46:20 -0500

    while menu

---
+++ 0929-estructuras_repetitivas_while/10-menu.py
@@ -0,0 +1,28 @@
+num1 = eval(input("Escribe un número: "))
+num2 = eval(input("Escribe otro número: "))
+
+opcion = 0
+
+while opcion < 1 or opcion > 4:
+    print("Menu")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("")
+    opcion = eval(input("Opción: "))
+
+if opcion == 1:
+    print("La suma es", num1 + num2)
+elif opcion == 2:
+    print("La resta es", num1 - num2)
+elif opcion == 3:
+    print("La multiplicación es", num1 * num2)
+elif opcion == 4:
+    if num2 == 0:
+        print("No se puede hacer la división.")
+    else:
+        print("La división es", num1 / num2)
+else:
+    print("Opción invalida.")
+
