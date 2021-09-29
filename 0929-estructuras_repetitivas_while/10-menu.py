6c2b9e457ef13525bda7cf78ab7d6b8d56ad9339
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-29 12:49:45 -0500

    while menu 2

--- 0929-estructuras_repetitivas_while/10-menu.py
+++ 0929-estructuras_repetitivas_while/10-menu.py
@@ -11,6 +11,9 @@ while opcion < 1 or opcion > 4:
     print("4. División")
     print("")
     opcion = eval(input("Opción: "))
+    if opcion < 1 or opcion > 4:
+        print("** OPCION INVALIDA **")
+        print("Ingresa de nuevo la opcion.")
 
 if opcion == 1:
     print("La suma es", num1 + num2)
