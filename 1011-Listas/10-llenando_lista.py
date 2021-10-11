201242d510715422da84091b479fc69051519cee
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-11 12:25:30 -0500

    listas 9

---
+++ 1011-Listas/10-llenando_lista.py
@@ -0,0 +1,10 @@
+lista = []
+
+fruta = "algo"
+
+while fruta != "":
+    fruta = input("Ingresa un nombre de una fruta: ")
+    if fruta != "":
+        lista.append(fruta)
+
+print("La lista es: ", lista)
