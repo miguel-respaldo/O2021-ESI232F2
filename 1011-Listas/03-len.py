29112614f1a1443b4f6e8af248159b3f925b18f0
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-11 11:38:17 -0500

    listas 4

--- 1011-Listas/03-len.py
+++ 1011-Listas/03-len.py
@@ -2,3 +2,4 @@ una_lista = ["manzana", "platano", "naranja"]
 #               0           1          2
 
 print("La lista tiene", len(una_lista), "elementos")
+print("El tipo es", type(una_lista))
---
+++ 1011-Listas/04-comando_list.py
@@ -0,0 +1,11 @@
+una_lista = list(("manzana", "platano", "naranja"))
+#                   0           1          2
+otra_lista = ["manzana", "platano", "naranja"]
+
+lista_vacia = []
+otra_lista_vacia = list()
+
+print(una_lista)
+print(otra_lista)
+print(lista_vacia)
+print(otra_lista_vacia)
