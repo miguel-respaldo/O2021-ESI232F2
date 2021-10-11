ddc2cd1d28868013ec14ac88bc92c1cd6ff1adf5
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-11 12:34:37 -0500

    listas 10

---
+++ 1011-Listas/11-producto_punto.py
@@ -0,0 +1,11 @@
+vector1 = [1, 2, 3]
+vector2 = [2, 2, 2]
+
+# Producto punto
+#  v1 = [a, b, c]
+#  v2 = [x, y, z]
+#  v1.v2 = a*x + b*y + c*z
+
+producto_punto = vector1[0]*vector2[0] + vector1[1]*vector2[1] + vector1[2]*vector2[2]
+
+print("El producto punto de", vector1, "con", vector2, "es", producto_punto)
