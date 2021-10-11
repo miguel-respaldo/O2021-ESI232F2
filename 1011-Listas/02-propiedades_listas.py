3c95b45480240922511bbe06a6632861da379f2c
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-11 11:28:37 -0500

    listas 2

---
+++ 1011-Listas/02-propiedades_listas.py
@@ -0,0 +1,7 @@
+una_lista = ["manzana", "platano", "naranja", "manzana"]
+#               0           1          2         3
+print("La lista:", una_lista)
+print("Elemento cuatro", una_lista[3])
+una_lista[3] = "melon"
+
+print("La lista:", una_lista)
