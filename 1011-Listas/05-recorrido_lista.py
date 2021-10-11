eafbfe987bc29f5bd3be6f3d848995f8d067b972
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-11 11:48:55 -0500

    listas 5

---
+++ 1011-Listas/05-recorrido_lista.py
@@ -0,0 +1,12 @@
+una_lista = ["manzana", "platano", "naranja", "kiwi", "melon"]
+#               0           1          2       3        4
+
+num_elementos = len(una_lista)
+
+for x in range(num_elementos):
+    print(una_lista[x])
+
+print("--------------------")
+
+for elemento in una_lista:
+    print(elemento)
