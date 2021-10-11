8651c87319af12536265a15722d309200d17dde7
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-11 12:13:05 -0500

    listas 7.1

--- 1011-Listas/08-verificar.py
+++ 1011-Listas/08-verificar.py
@@ -2,15 +2,15 @@ una_lista = ["manzana", "platano", "naranja", "limon", "cereza", "kiwi", "mango"
 #              0          1           2          3        4         5      6         7
 #              -8         -7          -6         -5       -4       -3      -2       -1
 
-if "manzana" in lista:
+if "manzana" in una_lista:
     print("Si, la manzana si esta en la lista")
 
 
-if "sandia" in lista:
+if "sandia" in una_lista:
     print("Si, la sandia esta en la lista")
 else:
     print("No, la sandia NO esta en la lista")
 
 
-if "caranbola" not in lista:
+if "caranbola" not in una_lista:
     print("La Caranbola No esta en la lista")
