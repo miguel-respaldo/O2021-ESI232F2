3314135f859b5c1bff16a9ac6d6d28fe5fccb68c
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-11 12:17:07 -0500

    listas 8

---
+++ 1011-Listas/09-append.py
@@ -0,0 +1,8 @@
+una_lista = ["manzana", "platano", "naranja", "limon"]
+
+print("La lista es:", una_lista)
+una_lista.append("kiwi")
+print("La lista es:", una_lista)
+una_lista.append("pera")
+print("La lista es:", una_lista)
+
