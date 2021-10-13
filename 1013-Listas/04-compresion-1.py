9cb6ff29aa2e397cde9f18f9c7b4936c66755ce8
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-13 11:50:17 -0500

    compresion

---
+++ 1013-Listas/04-compresion-1.py
@@ -0,0 +1,8 @@
+lista = ["manzana", "platano", "naranja", "limno", "kiwi"]
+lista_nueva = []
+
+for elemento in lista:
+    if "a" in elemento:
+        lista_nueva.append(elemento)
+        
+print(lista_nueva)
---
+++ 1013-Listas/05-compresion-2.py
@@ -0,0 +1,5 @@
+lista = ["manzana", "platano", "naranja", "limon", "kiwi"]
+
+lista_nueva = [elemento for elemento in lista if "a" in elemento]
+
+print(lista_nueva)
