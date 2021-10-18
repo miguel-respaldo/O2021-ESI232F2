0329b0a8f7e91a6e6bfef3fd38fa81b925315ce6
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-18 11:54:11 -0500

    funciones 6

---
+++ 1018-funciones/06-return-operaciones.py
@@ -0,0 +1,13 @@
+def suma(num1, num2):
+    return num1 + num2
+
+
+num1 = eval(input("Ingresa un número: "))
+num2 = eval(input("Ingresa otro número: "))
+
+# Regrese a una variable
+resultado = suma(num1, num2)
+print("La suma es:", resultado)
+
+# imprimir directo
+print("La suma es:", suma(num1, num2))
