5e7f70855c8aa6363116b7938e58c58e4d0c3102
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-18 11:27:44 -0500

    funciones

---
+++ 1018-funciones/01-funciones.py
@@ -0,0 +1,10 @@
+def nombre_de_funcion():
+    print("Hola desde una funcion")
+
+
+nombre_de_funcion()
+nombre_de_funcion()
+nombre_de_funcion()
+nombre_de_funcion()
+
+
---
+++ 1018-funciones/02-argumentos.py
@@ -0,0 +1,6 @@
+def nombre_completo(nombre):
+    print(nombre + " Lopez")
+
+nombre_completo("Emilio")
+nombre_completo("Luis")
+nombre_completo("Ana")
