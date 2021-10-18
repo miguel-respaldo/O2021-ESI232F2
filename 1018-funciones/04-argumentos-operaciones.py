0e3d8517a9a65f1d8ba80322b95313a275d7c3c8
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-18 11:43:46 -0500

    funciones 4

--- 1018-funciones/04-argumentos-operaciones.py
+++ 1018-funciones/04-argumentos-operaciones.py
@@ -4,8 +4,13 @@ def suma(*numeros):
 def resta(*numeros):
     print("La resta es:", numeros[0] - numeros[1])
 
+def multiplicacion(num1, num2):
+    print("La resta es:", num1 * num2)
+
+
 num1 = eval(input("Escribe un número: "))
 num2 = eval(input("Escribe otro número: "))
 
-suma(num1, num2)
-resta(num1, num2)
+suma(num1, num2, 3, 4, 5, 6)
+resta(num1, num2, 3, 4, 5)
+multiplicacion(num1, num2)
