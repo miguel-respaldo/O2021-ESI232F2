e5dfdec336d77adad9d3786c7f860ac4d84e442f
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-20 11:39:26 -0500

    ejercicio listas 2 2

---
+++ 1020-funciones_y_listas/05-inicializar-lista.py
@@ -0,0 +1,14 @@
+fruits = ['apple', 'banana', 'cherry']
+
+print("Lista de frutas", fruits)
+
+fruits = []
+
+print("Lista de furtas vacia:", fruits)
+
+fruits = ['apple', 'banana', 'cherry']
+print("Lista de frutas", fruits)
+
+fruits.clear()
+
+print("Lista de furtas vacia:", fruits)
