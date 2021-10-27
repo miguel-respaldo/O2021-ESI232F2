1f570c23f3b764d44b224dd12cb0058d1eff1deb
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-27 11:58:34 -0500

    archivos 7

---
+++ 1027-archivos/07-close.py
@@ -0,0 +1,13 @@
+archivo = open("demo.txt")
+
+print("Imprimo la primera linea:")
+print(archivo.readline())
+
+archivo.close()
+
+archivo = open("demo.txt")
+
+print("Imprimo la primera linea:")
+print(archivo.readline())
+
+archivo.close()
